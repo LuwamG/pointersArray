41c44e97ec056c7c9b447cb01ed02ca4c0f5c432
LuwamG <luwamgebrezgiabhier@outlook.com>
2024-10-14 21:40:38 -0700

    updated files

--- pointersArray/pointersArray.cpp
+++ pointersArray/pointersArray.cpp
@@ -1,24 +1,23 @@
-// this program uses pointers and arrays 
 #include <iostream>
 using namespace std;
-
-int main() {
+#include "pointersArray.hpp"
+// Function to read numbers into an array
+int* readNumbers(int& count) {
     int input;
-    int count = 0;
+    count = 0;
 
     cout << "Enter numbers (enter -1 to stop):" << endl;
- 
     while (true) {
         cin >> input;
-
-        
         if (input == -1) {
             break;
         }
-     count++; 
+        count++;
     }
 
+    // Create dynamic array
     int* arr = new int[count];
+
     cout << "Enter the numbers again:" << endl;
     int index = 0;
     while (index < count) {
@@ -26,19 +25,18 @@ int main() {
         if (input == -1) {
             break;
         }
-
-        arr[index++] = input; 
+        arr[index++] = input;
     }
 
-    // calculate the sum of the elements
+    return arr;
+}
+
+// Function to calculate the sum of the array elements
+int calculateSum(int* arr, int count) {
     int sum = 0;
     for (int i = 0; i < count; ++i) {
         sum += arr[i];
     }
-    cout << "The sum of the entered elements is: " << sum << endl;
-
-   
-    delete[] arr;
-
-    return 0;
+    return sum;
 }
+
---
+++ pointersArray/pointersArray.hpp
@@ -0,0 +1,7 @@
+#ifndef POINTERSARRAY_HPP
+#define POINTERSARRAY_HPP
+#include <iostream>
+using namespace std;
+int* readNumbers(int& count);
+int calculateSum(int* arr, int count);
+#endif
